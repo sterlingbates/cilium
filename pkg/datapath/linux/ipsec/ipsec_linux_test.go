d03d566c21d0021e71eb25294879bccf77444f32
John Fastabend <john.fastabend@gmail.com>
2023-08-04 23:13:14 -0700

    cilium/ipsec: only update cilium_encrypt_map after xfrm is configured
    From: John Fastabend <john.fastabend@gmail.com>
    
    The cilium_encrypt_map is used to determine what key the datapath should
    use. This is done by setting the mark value of the skb and then in the
    XFRM policy the mark value is matched to designate what encryption
    policy/state to use.
    
    However, on key rotation we have an issue where the map entry with the
    key is updated before the xfrm policy is plumbed. The result is its
    possible to mark the skb with a value that will have no matching
    xfrm policy and result in a policy block error and drop the skb.
    
    To resolve ensure we do setup in the correct order and only set the
    min key in the cilium_encrypt_map after the policy has been updated.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- pkg/datapath/linux/ipsec/ipsec_linux_test.go
+++ pkg/datapath/linux/ipsec/ipsec_linux_test.go
@@ -52,7 +52,9 @@ func (p *IPSecSuitePrivileged) TestLoadKeysNoFile(c *C) {
 
 func (p *IPSecSuitePrivileged) TestInvalidLoadKeys(c *C) {
 	keys := bytes.NewReader(invalidKeysDat)
-	_, _, err := LoadIPSecKeys(keys)
+	_, spi, err := LoadIPSecKeys(keys)
+	c.Assert(err, NotNil)
+	err = SetIPSecSPI(spi)
 	c.Assert(err, NotNil)
 
 	_, local, err := net.ParseCIDR("1.1.3.4/16")
@@ -66,10 +68,14 @@ func (p *IPSecSuitePrivileged) TestInvalidLoadKeys(c *C) {
 
 func (p *IPSecSuitePrivileged) TestLoadKeys(c *C) {
 	keys := bytes.NewReader(keysDat)
-	_, _, err := LoadIPSecKeys(keys)
+	_, spi, err := LoadIPSecKeys(keys)
+	c.Assert(err, IsNil)
+	err = SetIPSecSPI(spi)
 	c.Assert(err, IsNil)
 	keys = bytes.NewReader(keysAeadDat)
-	_, _, err = LoadIPSecKeys(keys)
+	_, spi, err = LoadIPSecKeys(keys)
+	c.Assert(err, IsNil)
+	err = SetIPSecSPI(spi)
 	c.Assert(err, IsNil)
 }
 
